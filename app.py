1c8fc3d3d6f9d3d7e3bc93cdd58eaa3a4060fe29
Evelyn Chang <evelyn68961@gmail.com>
2026-09-19 14:48:54 +0800

    multiple variables

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Evelyn")
+    return render_template("index.html", 
+                           name="Evelyn", 
+                           role="Pharmacist", 
+                           city="Taipei")
 
 @app.route("/dog")
 def dog():
